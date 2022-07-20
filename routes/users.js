51227b8d4d1d65960666dfb9147b4745706a15c4
Avishka Indula <avishkainduladev@gmail.com>
2022-07-20 08:57:18 +0530

    how to store files on a backend

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const multer = require("multer");
 
-const upload = multer({});
+const upload = multer({ dest: "images" });
 // We need to execute multer as a function first like this.
 // To this function, we need to pass a configuration object. {}
 // We can configure the behavior of multer through that.
 // For example, where it should store the files.
 // This will now give us a middleware named "upload"
 // Now we can apply this middleware to the route that handles the image upload.
+// We should define the path where the uploaded file goes by using dest: images
+// Now the uploaded files will be stored in the images folder.
+// Multer will automatically rename the uploaded files into a unique id to.
 
 const router = express.Router();
 
@@ -23,9 +26,29 @@ router.get("/new-user", function (req, res) {
 router.post("/profiles", upload.single("image"), function (req, res) {
   const uploadedImageFile = req.file;
   // This will hold the image JPG that was submitted
+  // req.file is an object that gives us extra file information like the path where the files are stored.
 
   const userData = req.body;
   // This will hold the username that was submitted through the form
+
+  console.log(uploadedImageFile);
+  // result==========================================================
+  // {
+  //   fieldname: 'image',
+  //   originalname: 'thomas shelby quotes 11.jpg',
+  //   encoding: '7bit',
+  //   mimetype: 'image/jpeg',
+  //   destination: 'images',
+  //   filename: '9fd305ea1adf16570af9e9101a6ea9ed',
+  //   path: 'images\\9fd305ea1adf16570af9e9101a6ea9ed',
+  //   size: 51367
+  // }
+
+  console.log(userData);
+  // result===========================================================
+  // { username: 'Avishka Indula' }
+
+  res.redirect("/");
 });
 // This is the route we create to accept the file uploads
 // So we need to apply the multer middleware to this route.
